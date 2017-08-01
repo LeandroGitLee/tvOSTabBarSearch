e581721e22489d2e25226a3e6b86025b16c5b048
Marcos Polanco <mjpolanco@gmail.com>
2017-08-01 10:59:51 -0400

    Initial Commit

---
+++ Search/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Search
+//
+//  Created by Marcos Polanco on 8/1/17.
+//  Copyright © 2017 Visor Labs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
